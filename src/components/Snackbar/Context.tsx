b695a94d6d55e4d395d905f8359972c4b5a0a48d
Kyle <thekyle@hotmail.com>
2019-11-02 20:34:56 +0900

    snackbar context

--- src/components/Snackbar/Context.tsx
+++ src/components/Snackbar/Context.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useContext, createContext } from 'react'
 
-export interface Snackbar {
-  snackbarState: { message: string; variant: string }
+interface Snackbar {
+  message: string
+  variant: string
+}
+
+interface SnackbarState {
+  snackbar: Snackbar
   setSnackbarState: React.Dispatch<React.SetStateAction<Snackbar>>
 }
 
-const defaultSnackbar = {
-  snackbarState: { message: '', variant: '' }
-  // setSnackbarState: React.Dispatch<React.SetStateAction<Snackbar>>
+const defaultSnackbar: Snackbar = {
+  message: '',
+  variant: ''
 }
 
-const SnackbarContext = React.createContext(defaultSnackbar)
+const defaultSnackbarState: SnackbarState = {
+  snackbar: defaultSnackbar,
+  setSnackbarState: (): void => {}
+}
 
-export default SnackbarContext
+export const SnackbarContext = createContext<SnackbarState>(
+  defaultSnackbarState
+)
+
+export const useProfileContext = (): SnackbarState => {
+  return useContext(SnackbarContext)
+}
+
+// export interface Snackbar {
+//   snackbarState: { message: string; variant: string }
+//   setSnackbarState: React.Dispatch<React.SetStateAction<Snackbar>>
+// }
+
+// const defaultSnackbar = {
+//   snackbarState: { message: '', variant: '' }
+//   // setSnackbarState: React.Dispatch<React.SetStateAction<Snackbar>>
+// }
+
+// const SnackbarContext = React.createContext(defaultSnackbar)
+
+// export default SnackbarContext

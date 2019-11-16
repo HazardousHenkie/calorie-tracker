ffd0e5559328a36ffc02d6239ddcb55bb9fc766b
Kyle <thekyle@hotmail.com>
2019-11-16 23:29:07 +0900

    change temporary name

--- src/pages/forgotPassword/index.tsx
+++ src/pages/forgotPassword/index.tsx
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
 
-import ForgotPassword from '../../components/forgotPassword'
+import ForgotPassword from '../../components/ForgotPassword1'
 
 const useStyles = makeStyles(theme => ({
   root: {

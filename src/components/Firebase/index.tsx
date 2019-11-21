efae5e6f2abb3821743496b5962f977642711508
Kyle <thekyle@hotmail.com>
2019-11-21 22:29:42 +0900

    local change fix

--- src/components/Firebase/index.tsx
+++
@@ -1,7 +0,0 @@
-import FirebaseContext, { withFirebase, FirebaseProviderProps } from './context'
-import Firebase from './firebase'
-
-export default Firebase
-
-export type FirebaseProviderProps = FirebaseProviderProps
-export { FirebaseContext, withFirebase }
